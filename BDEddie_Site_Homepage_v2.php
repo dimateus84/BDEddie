0f4abf4513d21de6569720bda8a9aced288bcab6
dimateus84 <stoppasivcom@yandex.ua>
2016-01-19 16:11:10 +0300

    add

--- BDEddie_Site_Homepage_v2.php
+++ BDEddie_Site_Homepage_v2.php
@@ -1,6 +1,6 @@
-<?php $title = 'name'; ?>
+<?php $title = 'BDEddie_Site_Homepage_v2'; ?>
 <?php include 'tpl/includes/head.inc'; ?>
-<body class="front page page-">
+<body class="front page page-home">
 <div class="outer-wrapper">
   <?php include 'tpl/layout/header.inc'; ?>
   <div class="inner-wrapper site-container">
