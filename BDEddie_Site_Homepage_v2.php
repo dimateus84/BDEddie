680414613ac9de21fdcafc41b354bc4dc9dc0ebe
dimateus84 <stoppasivcom@yandex.ua>
2016-01-20 12:39:35 +0300

    add slider

--- BDEddie_Site_Homepage_v2.php
+++ BDEddie_Site_Homepage_v2.php
@@ -3,8 +3,17 @@
 <body class="front page page-home">
 <div class="outer-wrapper">
   <?php include 'tpl/layout/header.inc'; ?>
+  <div class="b-slider">
+    <div class="flexslider">
+      <ul class="slides">
+        <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
+        <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
+      </ul>
+    </div>
+  </div>
   <div class="inner-wrapper site-container">
     <div class="content-wrapper">
+
 
     </div>
   </div>
