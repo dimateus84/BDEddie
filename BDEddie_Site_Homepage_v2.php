9fe2a6a96332e4c2614263e296cfbc16463f6c14
dimateus84 <stoppasivcom@yandex.ua>
2016-01-20 14:41:59 +0300

    add about

--- BDEddie_Site_Homepage_v2.php
+++ BDEddie_Site_Homepage_v2.php
@@ -13,8 +13,43 @@
   </div>
   <div class="inner-wrapper site-container">
     <div class="content-wrapper">
+      <div class="b-links">
+        <a href="#">
+          <img src="theme/images/tmp/link-work.jpg" alt="1"/>
+        </a>
+        <a href="#">
+          <img src="theme/images/tmp/link-shop.jpg" alt="2"/>
+        </a>
+        <a href="#">
+          <img src="theme/images/tmp/link-live.jpg" alt="3"/>
+        </a>
+      </div>
 
+      <div class="b-about-us">
+        <div class="col col-1">
+          <h2>about us</h2>
+        </div>
+        <div class="col col-2">
+          <p>What began over 100 years ago as a grocery and feed store, is now a real estate concern with a
+            portfolio that includes retail strip centers, fast food restaurants, convenience stores, office buildings,
+            and nearly 1,000 acres of land planned for future multi-use developments.
+          </p>
+        </div>
+        <div class="col col-3">
+          <p>One of our recent developments is Kelley Pointe, in Edmond, Oklahoma.
+            A combination of office, residential and retail uses is being developed for community-minded business
+            people.
+            Read more about Kelley Pointe and our premier office and retail space for lease in Edmond.
+          </p>
+        </div>
 
+      </div>
+
+      <div class="b-team">
+        <div class="col col-1"></div>
+        <div class="col col-2"></div>
+        <div class="col col-3"></div>
+      </div>
     </div>
   </div>
   <?php include 'tpl/layout/footer.inc'; ?>
