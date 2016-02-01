531b06690f19721d51bbff97888961447c15736e
dimateus84 <stoppasivcom@yandex.ua>
2016-02-01 14:57:52 +0300

    header refactor

--- BDEddie_Site_Homepage_v2.php
+++ BDEddie_Site_Homepage_v2.php
@@ -3,187 +3,190 @@
 <body class="front page page-home">
 <div class="outer-wrapper">
   <?php include 'tpl/layout/header.inc'; ?>
-  <div class="inner-wrapper site-container">
-    <div class="content-wrapper">
-      <div class="b-slider">
-        <div class="flexslider">
-          <ul class="slides">
-            <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
-            <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
-          </ul>
-        </div>
+  <div class="content-wrapper site-container">
+
+    <div class="b-slider width-padding">
+      <div class="flexslider">
+        <ul class="slides">
+          <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
+          <li><img src="theme/images/tmp/slider-image-1.jpg" alt=""/></li>
+        </ul>
       </div>
-      <div class="b-links">
-        <a href="#">
-          <img src="theme/images/tmp/link-work.jpg" alt="1"/>
-        </a>
-        <a href="#">
-          <img src="theme/images/tmp/link-shop.jpg" alt="2"/>
-        </a>
-        <a href="#">
-          <img src="theme/images/tmp/link-live.jpg" alt="3"/>
-        </a>
+    </div>
+
+    <div class="b-links width-padding">
+      <a href="#">
+        <img src="theme/images/tmp/link-work.jpg" alt="1"/>
+      </a>
+      <a href="#">
+        <img src="theme/images/tmp/link-shop.jpg" alt="2"/>
+      </a>
+      <a href="#">
+        <img src="theme/images/tmp/link-live.jpg" alt="3"/>
+      </a>
+    </div>
+
+    <div class="b-about-us width-padding">
+      <div class="col col-1">
+        <h2>about us.</h2>
       </div>
-
-      <div class="b-about-us">
-        <div class="col col-1">
-          <h2>about us.</h2>
-        </div>
-        <div class="col col-2">
-          <p>What began over 100 years ago as a grocery and feed store, is now a real estate concern with a
-            portfolio that includes retail strip centers, fast food restaurants, convenience stores, office buildings,
-            and nearly 1,000 acres of land planned for future multi-use developments.
-          </p>
-        </div>
-        <div class="col col-3">
-          <p>One of our recent developments is Kelley Pointe, in Edmond, Oklahoma.
-            A combination of office, residential and retail uses is being developed for community-minded business
-            people.
-            Read more about Kelley Pointe and our premier office and retail space for lease in Edmond.
-          </p>
-        </div>
-
+      <div class="col col-2">
+        <p>What began over 100 years ago as a grocery and feed store, is now a real estate concern with a
+          portfolio that includes retail strip centers, fast food restaurants, convenience stores, office buildings,
+          and nearly 1,000 acres of land planned for future multi-use developments.
+        </p>
       </div>
-
-      <div class="b-team">
-        <!--<div class="team-logo center">
-          <img src="theme/images/bg/the-team-logo.png" alt="team-logo"/>
-        </div>-->
-
-        <div class="photo-description">
-          <div class="left-part">
-            <img src="theme/images/tmp/portrait-1.jpg" alt="p1"/>
-
-            <h3>Terry McGuire</h3>
-
-            <p>Leasing Director</p>
-
-            <p>Terry has been with B.D. Eddie Enterprises for 16 years and has 20 years of experience in real estate
-              appraisal, management, leasing and development. Terry can help with office and retail space for lease in
-              Edmond and Oklahoma City, in addition to our commercial land available for development.
-            </p>
-          </div>
-          <div class="center">
-            <img src="theme/images/tmp/ampersand.png" alt="amp"/>
-          </div>
-          <div class="right-part">
-            <img src="theme/images/tmp/portrait-2.jpg" alt="p1"/>
-
-            <h3>Clay T. Farha</h3>
-
-            <p>President</p>
-
-            <p>Clay has been with B.D. Eddie Enterprises for 30 years and has earned a Certified Commercial Investment
-              Manager (CCIM) real estate designation.
-            </p>
-          </div>
-        </div>
-
-        <div class="contact">
-          <div class="center">
-
-            <a href="#">
-              <img src="theme/images/tmp/contanct.png" alt="cont"/>
-            </a>
-
-          </div>
-        </div>
+      <div class="col col-3">
+        <p>One of our recent developments is Kelley Pointe, in Edmond, Oklahoma.
+          A combination of office, residential and retail uses is being developed for community-minded business
+          people.
+          Read more about Kelley Pointe and our premier office and retail space for lease in Edmond.
+        </p>
       </div>
-
-
-      <div class="b-clients">
-        <div class="clients-logo">
-          <img src="theme/images/tmp/the-clients-logo.png" alt="cliens"/>
-        </div>
-        <div class="clients-list-wrapper">
-          <div class="left-part">
-            <h3>RETAIL</h3>
-
-            <div class="left-client-list-wrapper">
-
-              <div class="left-list">
-                <p>A&W Restaurants</p>
-
-                <p>Barnes & Noble</p>
-
-                <p>Big Sky Bread</p>
-
-                <p>Church's Chicken</p>
-
-                <p>Circle K</p>
-
-                <p>City Bites</p>
-
-                <p>Freddy's Frozen Custard</p>
-
-                <p>KFC</p>
-
-                <p>Long John Silver's</p>
-
-                <p>Mobile Macsters</p>
-
-                <p>Olive + Co</p>
-              </div>
-
-              <div class="right-list">
-                <p>Options 2 Design</p>
-
-                <p>Pack N' Ship</p>
-
-                <p>Paper N' More</p>
-
-                <p>Perk Place Cafe</p>
-
-                <p>Pole Position Raceway</p>
-
-                <p>Prim</p>
-
-                <p>ProCleaners</p>
-
-                <p>Snow Pea</p>
-
-                <p>Swiss Cleaners</p>
-
-                <p>Tony Foss Flowers</p>
-
-              </div>
-            </div>
-
-
-          </div>
-          <div class="right-pert">
-            <h3>OFFICE</h3>
-
-            <p>American Eagle Title Co.</p>
-
-            <p>Breast Imaging of Oklahoma</p>
-
-            <p>The Cotton Law Firm</p>
-
-            <p>Edward Jones</p>
-
-            <p>EMSA</p>
-
-            <p>Farmer's Insurance/Mike </p>
-
-            <p>Gaddis Agency</p>
-
-            <p>The Hays Group, Inc.</p>
-
-            <p>Michael Bay and Associates</p>
-
-            <p>Nick Massey Householder </p>
-
-            <p>Group Financial</p>
-
-            <p>Quinstreet/Surehits</p>
-
-          </div>
-        </div>
-      </div>
-
 
     </div>
+
+    <div class="b-team width-padding">
+
+      <div class="photo-description">
+        <div class="left-part">
+          <img src="theme/images/tmp/portrait-1.jpg" alt="p1"/>
+
+          <h3>Terry McGuire</h3>
+
+          <p>Leasing Director</p>
+
+          <p>Terry has been with B.D. Eddie Enterprises for 16 years and has 20 years of experience in real estate
+            appraisal, management, leasing and development. Terry can help with office and retail space for lease in
+            Edmond and Oklahoma City, in addition to our commercial land available for development.
+          </p>
+        </div>
+        <div class="center">
+          <img src="theme/images/tmp/ampersand.png" alt="amp"/>
+        </div>
+        <div class="right-part">
+          <img src="theme/images/tmp/portrait-2.jpg" alt="p1"/>
+
+          <h3>Clay T. Farha</h3>
+
+          <p>President</p>
+
+          <p>Clay has been with B.D. Eddie Enterprises for 30 years and has earned a Certified Commercial Investment
+            Manager (CCIM) real estate designation.
+          </p>
+        </div>
+      </div>
+
+      <div class="contact">
+        <div class="center">
+
+          <a href="#">
+            <img src="theme/images/tmp/contanct.png" alt="cont"/>
+          </a>
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="b-clients width-padding">
+      <div class="clients-logo">
+        <img src="theme/images/tmp/the-clients-logo.png" alt="cliens"/>
+      </div>
+      <div class="clients-list-wrapper">
+        <div class="left-part">
+          <h3>RETAIL</h3>
+
+          <div class="left-client-list-wrapper">
+
+            <div class="left-list">
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              
+              <p>A&W Restaurants</p>
+
+              <p>Barnes & Noble</p>
+
+              <p>Big Sky Bread</p>
+
+              <p>Church's Chicken</p>
+
+              <p>Circle K</p>
+
+              <p>City Bites</p>
+
+              <p>Freddy's Frozen Custard</p>
+
+              <p>KFC</p>
+
+              <p>Long John Silver's</p>
+
+              <p>Mobile Macsters</p>
+
+              <p>Olive + Co</p>
+            </div>
+
+            <div class="right-list">
+              <p>Options 2 Design</p>
+
+              <p>Pack N' Ship</p>
+
+              <p>Paper N' More</p>
+
+              <p>Perk Place Cafe</p>
+
+              <p>Pole Position Raceway</p>
+
+              <p>Prim</p>
+
+              <p>ProCleaners</p>
+
+              <p>Snow Pea</p>
+
+              <p>Swiss Cleaners</p>
+
+              <p>Tony Foss Flowers</p>
+
+            </div>
+          </div>
+
+
+        </div>
+        <div class="right-pert">
+          <h3>OFFICE</h3>
+
+          <p>American Eagle Title Co.</p>
+
+          <p>Breast Imaging of Oklahoma</p>
+
+          <p>The Cotton Law Firm</p>
+
+          <p>Edward Jones</p>
+
+          <p>EMSA</p>
+
+          <p>Farmer's Insurance/Mike </p>
+
+          <p>Gaddis Agency</p>
+
+          <p>The Hays Group, Inc.</p>
+
+          <p>Michael Bay and Associates</p>
+
+          <p>Nick Massey Householder </p>
+
+          <p>Group Financial</p>
+
+          <p>Quinstreet/Surehits</p>
+
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </div>
 <?php include 'tpl/layout/footer.inc'; ?>
